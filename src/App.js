b62ccc80464d4fa4441272c7adbe177019fa7e81
Mikio Crosby <mikio.crosby@gmail.com>
2021-10-26 17:18:38 +0900

    init: first push with quick demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+import { useWeb3React } from '@web3-react/core';
+import { InjectedConnector } from '@web3-react/injected-connector';
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
+import { WalletLinkConnector } from '@web3-react/walletlink-connector';
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+const metaMask = new InjectedConnector({
+  supportedChainIds: [1, 3],
+})
+
+const walletConnect = new WalletConnectConnector({
+  supportedChainIds: [1, 3],
+  qrcode: true
+})
+
+const coinbase = new WalletLinkConnector({
+  supportedChainIds: [1, 3],
+})
+
+
 
 function App() {
+  const { active, account, library, connector, activate, deactivate } = useWeb3React()
+  { active ? console.log(library) : console.log('wallet not connected yet') }
+
+  async function connectMM() {
+    try {
+      await activate(metaMask)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+
+  async function connectWC() {
+    try {
+      await activate(walletConnect)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  async function connectCB() {
+    try {
+      await activate(coinbase)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  async function handleDisconnect() {
+    deactivate()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <h2>Multiple Wallet connection playground</h2>
+      {active ? <h3>Connected account: {account}</h3> : <h3>Please use a method below to connect.</h3>}
+      <button onClick={connectMM}>MetaMask</button>
+      <button onClick={connectWC}>Wallet Connect</button>
+      <button onClick={connectCB}>Coinbase</button>
+      <br />
+      {active ? <button onClick={handleDisconnect}>Disconnect</button> : ''}
+    </AppContainer>
   );
 }
 
